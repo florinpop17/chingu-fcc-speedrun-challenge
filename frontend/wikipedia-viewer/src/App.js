763a3993fd8400760e55e6966ef4d6e98db26436
Florin Pop <popflorin1705@yahoo.com>
2017-04-23 18:46:21 +0300

    merged with master

--- frontend/wikipedia-viewer/src/App.js
+++ frontend/wikipedia-viewer/src/App.js
@@ -2,16 +2,35 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      wiki : undefined,
+      wikiSimple: undefined
+    }
+
+    this.getWiki = this.getWiki.bind(this);
+  }
+  getWiki() {
+    let title = 'Cristiano'
+    fetch(`https://en.wikipedia.org/w/api.php?action=query&titles=${title}&format=json&origin=*`)
+    .then((response) => {
+      this.setState({wikiSimple:response});
+      return response.json()
+    }).then((response) => {
+      this.setState({wiki:response});
+    })
+  }
   render() {
+
+    let {wiki, wikiSimple} = this.state;
+
+    console.log(wiki);
+    console.log(wikiSimple);
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <button onClick={ this.getWiki }>Get wiki</button>
       </div>
     );
   }
